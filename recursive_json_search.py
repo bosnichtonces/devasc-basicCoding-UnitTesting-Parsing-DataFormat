b7941116d81d28118c1deb2503f2274e42c07c6c
bosnichtonces <58177348+bosnichtonces@users.noreply.github.com>
2022-10-16 15:30:29 +0000

    Add files via upload

---
+++ recursive_json_search.py
@@ -0,0 +1,23 @@
+from test_data import *
+
+def json_search(key,input_object):
+    ret_val=[]
+    def inner_function(key,input_object):
+        if isinstance(input_object, dict):
+            for k, v in input_object.items(): 
+                if k == key:
+                    temp={k:v}
+                    ret_val.append(temp)
+                if isinstance(v, dict): 
+                    json_search(key,v)
+                elif isinstance(v, list): 
+                    for item in v:
+                        if not isinstance(item, (str,int)): 
+                            json_search(key,item)
+        else: 
+            for val in input_object:
+                if not isinstance(val, (str,int)):
+                    json_search(key,val)
+    inner_function(key,input_object)
+    return ret_val
+print(json_search("issueSummary",data))
